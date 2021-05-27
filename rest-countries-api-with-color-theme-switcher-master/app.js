993c01aeb9194e3cb12f6e1d8585af581bb0bb43
ScarSymmetry <caparisonhgs@gmail.com>
2021-05-27 03:06:55 +0300

    initial

---
+++ rest-countries-api-with-color-theme-switcher-master/app.js
@@ -0,0 +1,125 @@
+import "regenerator-runtime/runtime";
+import {
+	commaSeparated,
+	getDetailedFetch,
+	openModal,
+	modalContainer,
+	modal,
+} from "./helpers";
+
+const container = document.querySelector(".container");
+const input = document.querySelector(".search-field");
+const select = document.querySelector(".drop-list");
+const title = document.querySelector(".title");
+const themeToggler = document.querySelector(".toggler");
+const togglerText = document.querySelector(".toggler__mode");
+
+let searchLetter = "";
+
+const countriesData = fetch(
+	"https://restcountries.eu/rest/v2/all?fields=alpha3Code;flag;name;capital;region;population"
+)
+	.then((response) => response.json())
+	.then((data) => data);
+
+(async () => {
+	try {
+		renderCountry(await countriesData);
+	} catch (e) {
+		alert(e.message);
+	}
+})();
+
+const searchFilter = async () => {
+	const raw = await countriesData;
+
+	const filtered = raw.filter((data) =>
+		data.name.toLowerCase().includes(searchLetter.toLowerCase())
+	);
+
+	renderCountry(filtered);
+};
+
+const selectRegion = async (region) => {
+	const selectRegion = await countriesData;
+	const shit = selectRegion.filter((data) => data.region === region);
+	container.innerHTML = "";
+	renderCountry(shit);
+};
+
+async function handleCountryClick(countryCode) {
+	const countryData = await getDetailedFetch(countryCode);
+	const allCountries = await countriesData;
+	modal.style.display = "flex";
+	openModal(countryData, allCountries);
+}
+
+function renderCountry(data) {
+	data.forEach((data) => {
+		const countryCard = document.createElement("div");
+		countryCard.classList.add("container__card");
+
+		countryCard.innerHTML = `
+		<div class="country-card" data-code=${data.alpha3Code}>
+		<div class="country-card__wrapper">
+		<img src=${data.flag} alt="flag" class="country-card__flag">
+		</div>
+		 
+			
+            <div class="country-card__info">
+                <h3 class="name">${data.name}</h3>
+                <p class="population">Population:${commaSeparated(
+									data.population
+								)}</p>
+                <p class="region">Region:${data.region}</p>
+                <p class="capital">Capital:${data.capital}</p>
+		</div>
+		`;
+
+		container.appendChild(countryCard);
+	});
+}
+
+themeToggler.addEventListener("click", () => {
+	document.body.classList.toggle("light");
+	document.body.classList.contains("light")
+		? (togglerText.textContent = "Dark Mode")
+		: (togglerText.textContent = "Light Mode");
+});
+
+document.addEventListener("click", (e) => {
+	if (e.target.className === "country-card__flag") {
+		const testshit = e.target.parentElement.parentElement.dataset.code;
+
+		handleCountryClick(testshit);
+	}
+});
+
+modalContainer.addEventListener("click", (e) => {
+	const countryCode = e.target.dataset.code;
+	if (e.target.classList.contains("border-button")) {
+		handleCountryClick(countryCode);
+	}
+});
+
+title.addEventListener("click", () => {
+	document.location.href = "/";
+});
+
+select.addEventListener("change", (e) => {
+	input.value = "";
+	const region = e.target.value;
+
+	selectRegion(region);
+});
+
+input.addEventListener("input", (e) => {
+	select.value = "";
+
+	searchLetter = e.target.value;
+	const validate = /^[a-zA-Z]+$/;
+	if (e.target.value.match(validate)) {
+		container.innerHTML = "";
+		searchFilter();
+	}
+});
---
+++ rest-countries-api-with-color-theme-switcher-master/helpers.js
@@ -0,0 +1,82 @@
+export const modalContainer = document.querySelector(".modal__container");
+export const modal = document.querySelector(".modal");
+
+export async function getDetailedFetch(countryCode) {
+	try {
+		const detailedFetch = await fetch(
+			`https://restcountries.eu/rest/v2/alpha/${countryCode}?fields=name;nativeName;population;region;subregion;capital;topLevelDomain;currencies;languages;borders;flag`
+		);
+		const jsonCountry = await detailedFetch.json();
+
+		return jsonCountry;
+	} catch (err) {
+		alert(err.message);
+	}
+}
+
+export function commaSeparated(num) {
+	return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
+export function openModal(countryData, allCountriesData) {
+	const {
+		name,
+		nativeName,
+		population,
+		region,
+		subregion,
+		capital,
+		topLevelDomain,
+		currencies,
+		languages,
+		borders,
+		flag,
+	} = countryData;
+
+	const renderBorders = allCountriesData
+		.filter((country) => borders.includes(country.alpha3Code))
+		.map((border) => {
+			return `<button class="border-button" data-code=${border.alpha3Code}>${border.name}</button>`;
+		})
+		.join(" ");
+
+	modalContainer.innerHTML = `
+	
+
+	<div class="modal-imagebox">
+	<img src=${flag} alt="" class="modal__image">
+	</div>
+	  
+	      <div class="modal-data">
+        <div class="modal-textleft">
+
+            <h2 class="modal__country">${name}</h2>
+
+            <p class="modal__native">Native Name: <span>${nativeName}</span></p>
+            <p class="modal__population">Population:<span> ${commaSeparated(
+							population
+						)}</span></p>
+            <p class="modal__region">Region: <span>${region}</span></p>
+            <p class="modal__subregion">Sub Region: <span>${subregion}</span></p>
+            <p class="modal__capital">Capital: <span>${capital}</span></p>
+        </div>
+
+        <div class="modal-textright">
+            <p class="modal__domain">Top Level Domain:<span> ${topLevelDomain}</span></p>
+            <p class="modal__currencies">Currencies: <span>${currencies.map(
+							(currency) => currency.code
+						)}</span></p>
+            <p class="modal__languages">Languages: <span>${languages.map(
+							(language) => language.name
+						)}</span></p>
+
+            <div class="borders">Borders:${ borders.length > 0 ? renderBorders : "This country is lonely AF"}</div>
+        </div>
+    </div>
+			
+              
+				
+	`;
+
+	modal.appendChild(modalContainer);
+}
